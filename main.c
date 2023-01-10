fa62d47b394c46a278b76e99b2069331a0be34b4
shorv <kontakt.shorv@gmail.com>
2023-01-10 15:51:36 +0100

    add test function calls in main method

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include "disk_parser.h"
 
 int main() {
-    printf("Hello, World!\n");
+    struct disk_t* disk = disk_open_from_file("sail_fat16_volume.img");
+    struct volume_t* volume = fat_open(disk, 0);
+
+    fat_close(volume);
+    disk_close(disk);
     return 0;
 }
